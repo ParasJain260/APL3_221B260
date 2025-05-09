70a2f1fb3e696cb1a2028bfb75be6b4116af217e
221B260 <221B260@juet.co.in>
2025-05-09 14:58:40 +0530

    le8

---
+++ le8/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry() {
+        System.out.println("crawing");   
+    }
+    public void fly(){
+        System.out.println("singing a song");
+    }
+    public void eat() {
+        System.out.println("eats");
+    }
+}
---
+++ le8/IndianSwan.java
@@ -0,0 +1,11 @@
+public class IndianSwan implements Swan{
+    public void swim() {
+        System.out.println("swims");
+    }
+    public void sing(){
+        System.out.println("sings");
+    }
+    public void eat() {
+        System.out.println("eats a fish");
+    }
+}
